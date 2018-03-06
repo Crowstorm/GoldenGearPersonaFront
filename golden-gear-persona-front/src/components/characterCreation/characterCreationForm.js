85f5714c107d5670c503d467083900d89317a245
Mateusz Mazur <crowstormofficial@gmail.com>
2018-03-06 10:29:49 +0100

    form changes

--- golden-gear-persona-front/src/components/characterCreation/characterCreationForm.js
+++ golden-gear-persona-front/src/components/characterCreation/characterCreationForm.js
@@ -1,26 +1,30 @@
 import React from 'react';
+import _ from 'lodash';
 import { reduxForm, Field } from 'redux-form';
+import {Link} from 'react-router-dom'
 
 import characterCreationFormField from './characterCreationFormField';
+
+const FIELDS = [
+    { label: "Character Name", name: "name" },
+    { label: "Character Title", name: "title" }
+]
 
 
 class CharacterCreationForm extends React.Component {
     renderFields() {
-        return (
-            <div>
-                <Field label="Character Name" type="text" name="Character Name" component={characterCreationFormField} />
-                <Field label="Character Title" type="text" name="Character Title" component={characterCreationFormField} />
-            </div>
-        )
+        return _.map(FIELDS, field => {
+            return <Field key={field.name} component={characterCreationFormField} type="text" label={field.label} name={field.name}/>
+        });
     }
 
     render() {
         return (
-            <div>
+            <div className="container">
                 <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
                     {this.renderFields()}
-
-                    <button type="submit">Submit</button>
+                    <Link className="red btn-flat white-text" to='/'>Cancel </Link>
+                    <button className="teal btn-flat right white-text" type="submit">Next</button>
                 </form>
             </div>
         )
