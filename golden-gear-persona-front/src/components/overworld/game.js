0a5d6f7b391ef4d0df84b47cbdd8afb9413ba12c
Mateusz Mazur <crowstormofficial@gmail.com>
2018-03-07 10:16:45 +0100

    grid test

--- golden-gear-persona-front/src/components/overworld/game.js
+++ golden-gear-persona-front/src/components/overworld/game.js
@@ -1,10 +1,30 @@
 import React from 'react';
-import './game.css'
+import _ from 'lodash';
+import './game.css';
 
-class Game extends React.Component{
-    render(){
-        return(
-            <div className="game">Gejm</div>
+
+const GRID = [
+    [{ x: 1, y: 3 }, { x: 2, y: 3 }, { x: 3, y: 3 }],
+    [{ x: 1, y: 2 }, { x: 2, y: 2 }, { x: 3, y: 2 }],
+    [{ x: 1, y: 1 }, { x: 2, y: 1 }, { x: 3, y: 1 }],
+]
+class Game extends React.Component {
+    renderGrid() {
+        return _.map(GRID, row => {
+            return <div className="row"> {_.map(row, cell => {
+                return <div style={{ height: 100 + 'px', width: 100 + 'px' }}> x: {cell.x}, y: {cell.y} </div>
+            })
+            } </div>
+        })
+    }
+
+    render() {
+        return (
+            <div className="game">
+                <div className="container">
+                    {this.renderGrid()}
+                </div>
+            </div>
         )
     }
 }
